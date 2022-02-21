83ee4040dc6ce65414bd9812ca561ab131a6af3a
Aaadarsh <58807249+hupu496@users.noreply.github.com>
2022-02-21 08:28:56 -0800

    done

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,21 +1,54 @@
 <?php
-
+$insert = false;
+$update = false;
+$delete = false;
 $servername="localhost";
 $username = "root";
 $password = "";
 $database = "notes";
 
 $conn= mysqli_connect($servername,$username,$password,$database);
-if($conn){
+if(!$conn){
     die("sorry wo failed to connect". mysqli_connect_error());
 }
-else {
-    echo "Connection was successful<br>";
+if(isset($_GET['delete'])){
+  $sno = $_GET['delete'];
+  $delete = true;
+  $sql = "DELETE FROM `nte` WHERE `pid` = $sno";
+  $result = mysqli_query($conn, $sql);
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  if(isset($_POST['snoEdit'])){
+    $sno = $_POST['snoEdit'];
+    $title = $_POST['titleEdit'];
+    $description = $_POST['descriptionEdit'];
+    $sql = "UPDATE `nte` SET `Title` = '$title' , `description` = '$description' WHERE `nte`.`pid` = '$sno'";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+      $update = true;
+    }
+    else{
+      echo "we could not update successfully";
+    }
+  }
+  else{
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $sql = "INSERT INTO `nte` (`Title`, `description`) VALUES ('$title','$description')";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+      $insert = true;
+    }
+    else{
+      echo "The record was not inserted in database". mysqli_error($conn);
+    }
+  }
 }
 
 
-?>
+ 
 
+?>
 
 <!doctype html>
 <html lang="en">
@@ -25,11 +58,50 @@ else {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
     <title>crud</title>
   </head>
   <body>
+  <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editModalLabel">Edit this Note</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <form method="post" action="/php-tutorial/crud/index.php">
+          <div class="modal-body">
+          <input type="hidden" name="snoEdit" id="snoEdit">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Note Title</label>
+    <input type="text" name="titleEdit" class="form-control" id="titleEdit" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Note Description</label>
+    <input type="textarea" name="descriptionEdit" class="form-control" id="descriptionEdit">
+  </div>
+          </div>
+        
+  
+  
+          <div class="modal-footer d-block mr-auto">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+          </form>
+      </div>
+    </div>
+  </div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Crud-PHP</a>
@@ -56,33 +128,131 @@ else {
     </div>
   </div>
 </nav>
+<?php
+
+if($insert){
+  echo "<div class='alert alert-success' role='alert'>
+  This is a success alert—check it out!
+</div>";
+}
+?>
+ <?php
+  if($delete){
+    echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your note has been deleted successfully
+    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>";
+  }
+  ?>
+   <?php
+  if($update){
+    echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your note has been update successfully
+    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>";
+  }
+  ?>
     <div class="container mt-3">
         <h2>Add a Note</h2>
-    <form method="post" action="">
+    <form method="post" action="/php-tutorial/crud/index.php">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Note Title</label>
-    <input type="text" name="note" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp">
     
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Note Description</label>
-    <input type="textarea" name="desc" class="form-control" id="exampleInputPassword1">
+    <input type="textarea" name="description" class="form-control" id="description">
   </div>
   
   <button type="submit" class="btn btn-primary">Add Note</button>
 </form>
     </div>
 
-    <div class="container">
-       <?php 
-       
+    <div class="container my-4">
+<table class="table" id="myTable">
+
+  <thead>
+    <tr>
+      <th scope="col">S.no</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <?php 
+       $sql = "SELECT * FROM `nte`";
+       $result = mysqli_query($conn , $sql);
+       $sno = 0;
+       while($row = mysqli_fetch_assoc($result)){
+         $sno=$sno + 1;
+         echo "<tr>
+         <th scope='row'>". $sno ."</th>
+         <td>". $row['Title'] ."</td>
+         <td>". $row['description'] ."</td>
+         <td><button class='edit btn btn-primary' id=". $row['pid'] .">EDIT</button> <button class='delete btn btn-danger' id=d". $row['pid'] ."> DELETE</button> </td>
+       </tr>";
+         echo "<br>";
+       }
        
        
        ?> 
+  </tbody>
+</table>
     </div>
+    <hr>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+  <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    
+  <script>$(document).ready( function () {
+    $('#myTable').DataTable();
+} );</script>
 
+<script>
+ edits = document.getElementsByClassName('edit');
+    Array.from(edits).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("edit ");
+        tr = e.target.parentNode.parentNode;
+        title = tr.getElementsByTagName("td")[0].innerText;
+        description = tr.getElementsByTagName("td")[1].innerText;
+        console.log(title, description);
+        titleEdit.value = title;
+        descriptionEdit.value = description;
+        snoEdit.value = e.target.id;
+        console.log(e.target.id);
+        $('#editModal').modal('toggle');
+        
+      })
+    })
+
+    deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("delete", );
+        sno =e.target.id.substr(1);
+        if(confirm("Are you want to delete the data!")){
+            window.location = `/php-tutorial/crud/index.php?delete=${sno}`;
+        }
+      })
+    })
+</script>
+ 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
